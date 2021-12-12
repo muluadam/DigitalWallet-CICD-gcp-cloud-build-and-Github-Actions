0fd8eaca2924204b85e6570cbd886d2215495bc4
Muluadam Temesgen Akalu <mtmuluadam08@gmail.com>
2021-12-11 21:55:38 -0600

    commit test from adam

--- src/main/java/com/digital/wallet/utils/ResponseMessage.java
+++ src/main/java/com/digital/wallet/utils/ResponseMessage.java
@@ -3,10 +3,9 @@ package com.digital.wallet.utils;
 import java.util.UUID;
 
 public class ResponseMessage {
-		   public static String generate() {
-		      // creating UUID      
-		      return UUID.fromString("38400000-8cf0-11bd-b23e-10b96e4ef00d").toString();     
-		       
-		}
+
+	public static void test(String[] args) {
+		System.out.println("this is tet");
+	}
 
 }
